4b383f99b8554d67328e6a158e43dc36c646e1ab
Administrator <Administrator@USER-PC>
2024-03-26 20:09:39 +0200

    Добавьте файлы проекта.

---
+++ progekt_1_Gamakharia/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progekt_1_Gamakharia
+{
+    internal class Product
+    {
+        private string type;
+        private string name;
+        private int production_date;
+        private int shelf_life;
+       
+        private int today_is_date ;
+
+
+        public Product(string type, string name, int production_date, int shelf_life,int today_is_date)
+        {
+            this.type = type;
+            this.name = name;
+            this.production_date = production_date;
+            this.shelf_life = shelf_life;
+          
+            this. today_is_date= today_is_date;
+        }
+        public void Productinfo()
+        {
+            Console.WriteLine($"type: {type}");
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"production_date: {production_date}");
+            Console.WriteLine($"shelf_life: {shelf_life}");
+           
+            Console.WriteLine($"today_is_date: {today_is_date}\n");
+        }
+        public void change_of_today_is_date(int today_is_date)
+        {
+            this.today_is_date = today_is_date;
+            Console.WriteLine("date changed\n");
+        }
+        public bool shelf_life_expired()
+        {
+            if (shelf_life< today_is_date- production_date) {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            } 
+
+        }
+
+
+    }
+}
---
+++ progekt_1_Gamakharia/Program.cs
@@ -0,0 +1,13 @@
+﻿using progekt_1_Gamakharia;
+var Product = new Product("beverage","milk",3,7,5);
+Product.Productinfo();
+Product.change_of_today_is_date(7);
+
+if (Product.shelf_life_expired()) {
+    Console.WriteLine("shelf life expired");
+}
+else
+{
+    Console.WriteLine("the product is edible");
+
+}
